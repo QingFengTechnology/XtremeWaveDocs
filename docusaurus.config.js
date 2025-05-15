ed46b2868df8ba7a6ed248d034355b899d018758
QingFeng-awa <qingfeng_awa@163.com>
2025-05-16 04:50:00 +0800

    提高模糊搜索距离

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,7 @@ const config = {
         searchBarShortcutHint: false,
         searchBarPosition: "right",
         forceIgnoreNoIndex: true,
+        fuzzyMatchingDistance: 3,
       }),
     ],
   ],
