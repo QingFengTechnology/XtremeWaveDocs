8996f3d7b7bc21f0d8023c8c33c91a02be005105
QingFeng <qingfeng_awa@163.com>
2025-05-25 17:26:39 +0800

    New translations zh.js (English)

--- docs/.vitepress/i18n/en.js
+++ docs/.vitepress/i18n/en.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vitepress';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar';
 export const zhLocaleConfig = defineConfig({
-  label: '简体中文',
-  lang: 'zh-CN',
-  title: 'XtremeWave模组文档',
-  titleTemplate: ':title - 极致文档',
-  description: 'XtremeWave模组文档,提供了XtremeWave旗下开源模组的详细文档,包括安装、教程、配置等。',
+  label: 'English',
+  lang: 'en-US',
+  title: 'XtremeWave Mod Docs',
+  titleTemplate: ':title - XtremeDocs',
+  description: 'XtremeWave Official Documentation for Mods.',
   themeConfig: {
-    siteTitle: '极致文档',
+    siteTitle: 'XtremeDocs',
     nav: [
       {
-        text: '首页',
+        text: 'Home',
         link: '/',
       },
       {
-        text: '关于',
+        text: 'About',
         link: '/about',
       },
     ],
@@ -25,25 +25,26 @@ export const zhLocaleConfig = defineConfig({
       },
     ]),
     outline: {
-      label: '页面导航',
+      label: 'On this page',
     },
     footer: {
-      message: "由<a href='https://www.rainyun.com/QingFeng_?s=XtremeDocs' target='_blank'>雨云</a>提供云服务",
+      message:
+        "Cloud services provided by <a href='https://www.rainyun.com/QingFeng_?s=XtremeDocs' target='_blank'>Rainyun</a>",
       copyright: "© 2023-2025 By <a href='https://qingfengawa.top' target='_blank'>QingFeng</a>",
     },
     editLink: {
       pattern: 'https://github.com/QingFengTechnology/XtremeWaveDocs/blob/v2/:path',
-      text: '在Github上查看此页',
+      text: 'View it on GitHub',
     },
     docFooter: {
       prev: false,
       next: false,
     },
-    darkModeSwitchLabel: '颜色模式',
-    lightModeSwitchTitle: '点击以切换至浅色模式',
-    darkModeSwitchTitle: '点击以切换至深色模式',
-    sidebarMenuLabel: '菜单',
-    returnToTopLabel: '返回顶部',
-    langMenuLabel: '语言',
+    darkModeSwitchLabel: 'Appearance',
+    lightModeSwitchTitle: 'Switch to light theme',
+    darkModeSwitchTitle: 'Switch to dark theme',
+    sidebarMenuLabel: 'Menu',
+    returnToTopLabel: 'Return to top',
+    langMenuLabel: 'Language',
   },
 });
