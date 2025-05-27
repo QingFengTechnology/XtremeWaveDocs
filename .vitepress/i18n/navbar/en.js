16facdb471104a8d772e4456b43d01a1e005664f
QingFeng <qingfeng_awa@163.com>
2025-05-27 23:38:47 +0800

    New translations zh.js (Multilingual)

---
+++ .vitepress/i18n/navbar/en.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress';
+export const zhNavConfig = defineConfig([
+    {
+        text: '首页',
+        link: '/',
+    },
+    {
+        text: '关于',
+        link: '/about',
+    },
+    {
+        text: '文档',
+        items: [
+            {
+                text: 'FinalSuspect',
+                link: '/FinalSuspect/',
+                activeMatch: '/FinalSuspect/',
+            },
+        ],
+    },
+]);
