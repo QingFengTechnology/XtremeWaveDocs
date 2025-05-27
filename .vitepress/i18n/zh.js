28991bd0a946869f29c3a5b80d043c5286302c0c
QingFeng <qingfeng_awa@163.com>
2025-05-27 22:41:44 +0800

    New translations zh.js (Multilingual)

--- .vitepress/i18n/zh.js
+++ .vitepress/i18n/zh.js
@@ -1,35 +1,34 @@
 import { defineConfig } from 'vitepress';
 import { zhNavConfig } from './navbar/zh.js';
 import { zhSidebarConfig } from './sidebar/zh/index.js';
-
 export const zhLocaleConfig = defineConfig({
   label: '简体中文',
   lang: 'zh-CN',
-  title: "XtremeWave模组文档",
-  titleTemplate: ":title - 极致文档",
-  description: "XtremeWave模组文档,提供了XtremeWave旗下开源模组的详细文档,包括安装、教程、配置等。",
+  title: 'XtremeWave模组文档',
+  titleTemplate: ':title - 极致文档',
+  description: 'XtremeWave模组文档,提供了XtremeWave旗下开源模组的详细文档,包括安装、教程、配置等。',
   themeConfig: {
-    siteTitle: "极致文档",
+    siteTitle: '极致文档',
     nav: zhNavConfig,
     sidebar: zhSidebarConfig,
     outline: {
-      label: "页面导航"
+      label: '页面导航',
     },
     editLink: {
       // 目前pattern在`config.mjs`中定义后,若不在语言配置中定义会导致pattern无法生效。
       // 这似乎与VitePress所说的`浅层合并`(也许是我理解错误?)不符。
       pattern: 'https://github.com/QingFengTechnology/XtremeWaveDocs/blob/v2/docs/:path',
-      text: '在Github上查看此页'
+      text: '在Github上查看此页',
     },
     docFooter: {
       prev: false,
-      next: false
+      next: false,
     },
-    darkModeSwitchLabel: "颜色模式",
-    lightModeSwitchTitle: "点击以切换至浅色模式",
-    darkModeSwitchTitle: "点击以切换至深色模式",
-    sidebarMenuLabel: "菜单",
-    returnToTopLabel: "返回顶部",
-    langMenuLabel: "语言"
-  }
-})
+    darkModeSwitchLabel: '颜色模式',
+    lightModeSwitchTitle: '点击以切换至浅色模式',
+    darkModeSwitchTitle: '点击以切换至深色模式',
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '返回顶部',
+    langMenuLabel: '语言',
+  },
+});
