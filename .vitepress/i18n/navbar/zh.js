1568ba93955704ed0eeb6872254e2bedb8550936
QingFeng <qingfeng_awa@163.com>
2025-05-27 22:48:28 +0800

    Update source file zh.js

--- .vitepress/i18n/navbar/zh.js
+++ .vitepress/i18n/navbar/zh.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vitepress';
+
 export const zhNavConfig = defineConfig([
-    {
-        text: '首页',
-        link: '/',
-    },
-    {
-        text: '关于',
-        link: '/about',
-    },
-    {
-        text: '文档',
-        items: [
-            {
-                text: 'FinalSuspect',
-                link: '/FinalSuspect/',
-                activeMatch: '/FinalSuspect/',
-            },
-        ],
-    },
+  { text: '首页', link: '/' },
+  { text: '关于', link: '/about' },
+  {
+    text: '文档',
+    items: [
+      { text: 'FinalSuspect', link: '/FinalSuspect/', activeMatch: '/FinalSuspect/' },
+    ]
+  }
 ]);
