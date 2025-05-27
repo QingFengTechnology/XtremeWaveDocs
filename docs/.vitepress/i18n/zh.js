ebd6c979a8762bc0f47c8f41178103dc6de515f8
QingFeng <qingfeng_awa@163.com>
2025-05-27 19:42:31 +0800

    Update source file zh.js

--- docs/.vitepress/i18n/zh.js
+++ docs/.vitepress/i18n/zh.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vitepress';
+import { zhNavConfig } from './navbar/zh.js';
+import { zhSidebarConfig } from './sidebar/zh/index.js';
 
 export const zhLocaleConfig = defineConfig({
   label: '简体中文',
@@ -8,29 +10,15 @@ export const zhLocaleConfig = defineConfig({
   description: "XtremeWave模组文档,提供了XtremeWave旗下开源模组的详细文档,包括安装、教程、配置等。",
   themeConfig: {
     siteTitle: "极致文档",
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '关于', link: '/about' },
-      {
-        text: '文档',
-        items: [
-          { 
-            text: 'FinalSuspect',
-            items: [
-            ] 
-          },
-        ]
-      }
-    ],
+    nav: zhNavConfig,
+    sidebar: zhSidebarConfig,
     outline: {
       label: "页面导航"
     },
-    footer: {
-      message: "由<a href='https://www.rainyun.com/QingFeng_?s=XtremeDocs' target='_blank'>雨云</a>提供云服务",
-      copyright: "© 2023-2025 By <a href='https://qingfengawa.top' target='_blank'>QingFeng</a>"
-    },
     editLink: {
-      pattern: 'https://github.com/QingFengTechnology/XtremeWaveDocs/blob/v2/:path',
+      // 目前pattern在`config.mjs`中定义后,若不在语言配置中定义会导致pattern无法生效。
+      // 这似乎与VitePress所说的`浅层合并`(也许是我理解错误?)不符。
+      pattern: 'https://github.com/QingFengTechnology/XtremeWaveDocs/blob/v2/docs/:path',
       text: '在Github上查看此页'
     },
     docFooter: {
